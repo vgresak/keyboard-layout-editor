df961215eba3fe892ad389039c1320b2f2602322
Viktor Grešák <v.gresak@gmail.com>
2019-04-15 21:02:45 +0200

    added url tooltip to links, try to use xdg-open as a fallback when HostServices are not supported by the system

--- src/main/java/cz/gresak/keyboardeditor/component/WebsiteLink.java
+++ src/main/java/cz/gresak/keyboardeditor/component/WebsiteLink.java
@@ -1,14 +1,27 @@
 package cz.gresak.keyboardeditor.component;
 
 import cz.gresak.keyboardeditor.service.ServiceLoader;
+import cz.gresak.keyboardeditor.service.api.CommandExecutor;
 import javafx.application.HostServices;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Tooltip;
 
 public class WebsiteLink extends Hyperlink {
     private static final HostServices HOST_SERVICES = ServiceLoader.lookup(HostServices.class);
+    private static final CommandExecutor COMMAND_EXECUTOR = ServiceLoader.lookup(CommandExecutor.class);
 
     public WebsiteLink(String text, String url) {
         super(text);
-        setOnAction(event -> HOST_SERVICES.showDocument(url));
+        setTooltip(new Tooltip(url));
+        setOnAction(event -> openInBrowser(url));
+    }
+
+    private void openInBrowser(String url) {
+        try {
+            HOST_SERVICES.showDocument(url);
+        } catch (NullPointerException npe) {
+            // HostServices are not available, try xdg-open
+            COMMAND_EXECUTOR.execute(String.format("xdg-open %s", url));
+        }
     }
 }
