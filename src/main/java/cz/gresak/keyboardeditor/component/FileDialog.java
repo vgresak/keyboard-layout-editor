7afa46e71c3eae4615acd41af383d3837defb95e
Viktor Grešák <v.gresak@gmail.com>
2019-04-25 15:40:00 +0200

    Improved save file dialog - default file name, initial dir set to location of the application

--- src/main/java/cz/gresak/keyboardeditor/component/FileDialog.java
+++ src/main/java/cz/gresak/keyboardeditor/component/FileDialog.java
@@ -4,14 +4,24 @@ import javafx.stage.FileChooser;
 import javafx.stage.Window;
 
 import java.io.File;
+import java.net.URISyntaxException;
 
 public class FileDialog {
     private final static FileChooser fileChooser = new FileChooser();
 
     static {
-        File initialDir = new File(System.getProperty("user.home"));
-        if (initialDir.exists()) {
-            fileChooser.setInitialDirectory(initialDir);
+        fileChooser.setInitialFileName("custom");
+        try {
+            File sourceLocation = new File(FileDialog.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+            if (sourceLocation.exists()) {
+                if (!sourceLocation.isDirectory()) {
+                    // sourceLocation is JAR file, select parent dir
+                    sourceLocation = sourceLocation.getAbsoluteFile().getParentFile();
+                }
+                fileChooser.setInitialDirectory(sourceLocation);
+            }
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
         }
     }
 
