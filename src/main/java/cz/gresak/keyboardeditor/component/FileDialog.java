a23f08cb0344a6363b1498b6c11d62b2b4bedbaa
Viktor Grešák <v.gresak@gmail.com>
2019-05-01 16:27:27 +0200

    Initial export directory set to working directory

--- src/main/java/cz/gresak/keyboardeditor/component/FileDialog.java
+++ src/main/java/cz/gresak/keyboardeditor/component/FileDialog.java
@@ -4,24 +4,15 @@ import javafx.stage.FileChooser;
 import javafx.stage.Window;
 
 import java.io.File;
-import java.net.URISyntaxException;
 
 public class FileDialog {
     private final static FileChooser fileChooser = new FileChooser();
 
     static {
         fileChooser.setInitialFileName("custom");
-        try {
-            File sourceLocation = new File(FileDialog.class.getProtectionDomain().getCodeSource().getLocation().toURI());
-            if (sourceLocation.exists()) {
-                if (!sourceLocation.isDirectory()) {
-                    // sourceLocation is JAR file, select parent dir
-                    sourceLocation = sourceLocation.getAbsoluteFile().getParentFile();
-                }
-                fileChooser.setInitialDirectory(sourceLocation);
-            }
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
+        File initialDir = new File(System.getProperty("user.dir"));
+        if (initialDir.exists()) {
+            fileChooser.setInitialDirectory(initialDir);
         }
     }
 
